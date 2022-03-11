d7863ec90b8454f3594704084074b92f7ab558d7
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2022-03-10 23:31:47 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+  
+  //Attribute
+  
+  @EmbeddedId
+  private ScorePK id = new ScorePK();
+  
+  
+  private Double value;
+
+
+
+  //Constructor
+  public Score() {
+  } 
+
+
+  //Getter and Setter
+  public void setMovie(Movie movie) {
+    id.setMovie(movie);
+  }
+
+  public void setUser(User user) {
+    id.setUser(user);
+  }
+
+  public ScorePK getId() {
+    return id;
+  }
+
+  public void setId(ScorePK id) {
+    this.id = id;
+  }
+
+  public Double getValue() {
+    return value;
+  }
+
+  public void setValue(Double value) {
+    this.value = value;
+  }
+
+
+}
+
