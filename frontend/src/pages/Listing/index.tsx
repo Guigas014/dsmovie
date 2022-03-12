306b43447050851157dff069afc9d373de3b1305
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2022-03-12 18:45:57 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
-import Pagination from 'components/Pagination'
+import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
+import { BASE_URL } from 'utils/requests'
 
 import './styles.css';
 
 
 function Listing() {
+
+  //FORMA ERRADA
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(resposne => {
+          console.log(resposne.data)
+        })  
+
 
   return (
     <>
